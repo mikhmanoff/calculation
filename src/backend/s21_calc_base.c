46f0e5545f8dd28c6afd79711bc31140f5dfeb1f
sallieba <sallieba@student.21-school.ru>
2023-07-11 15:51:29 +0300

    new func

---
+++ src/backend/s21_calc_base.c
@@ -0,0 +1,76 @@
+#include "calc_support.h"
+
+void print(Node *list) {
+  if (list == NULL) {
+    printf("NULL\n");
+    exit(0);
+  }
+  for (Node *p = list; p != NULL; p = p->next) {
+    printf("Приоритет - %d\nЧисло - %lf\nТип - %d\n-------------------\n",
+           p->priority, p->num, p->funType);
+  }
+  printf("\n");
+}
+
+void push(Node **pList, double num, funcType funType, int priority) {
+  Node *p = malloc(sizeof(Node));
+  p->num = num;
+  p->priority = priority;
+  p->funType = funType;
+  p->next = *pList;
+  *pList = p;
+}
+
+void pop(Node **pList) {
+  if (*pList == NULL) {
+    exit(0);
+  }
+  Node *p = *pList;
+  *pList = p->next;
+  free(p);
+}
+
+int isEmpty(Node *List) { return List == NULL; }
+
+Node *inverseNode(Node *list) {
+  Node *tmp = NULL;
+
+  while (list != NULL) {
+    push(&tmp, list->num, list->funType, list->priority);
+    pop(&list);
+  }
+
+  return tmp;
+}
+
+// int main() {
+//   Node *list = NULL;
+
+//   push(&list, 14.48, tan_t, ULTRA_PRIORITY);
+//   push(&list, 23.8, sin_t, LOW_PRIORITY);
+//   push(&list, 4.4, cos_t, MEDIUM_PRIORITY);
+//   push(&list, 400.4, cos_t, LOW_PRIORITY);
+
+//   push(&list, 46.657, mod_t, LOW_PRIORITY);
+//   push(&list, 344.345, tan_t, HIGH_PRIORITY);
+//   push(&list, 23324.546, sin_t, ULTRA_PRIORITY);
+//   push(&list, 2323423.657, atan_t, MEDIUM_PRIORITY);
+
+//   //   print(list);
+//   //   printf("\n\n\n");
+
+//   //   print(list);
+
+//   //   pop(&list);
+//   //   printf("\n\n\n\n");
+
+//   //   print(list);
+//   while (!isEmpty(list)) {
+//     pop(&list);
+//     if (!isEmpty(list)) {
+//       printf("%lf\n", list->num);
+//     };
+//   }
+
+//   return 0;
+// }
---
+++ src/backend/s21_calc_preprocess.c
@@ -0,0 +1,129 @@
+#include "calc_support.h"
+
+Node *parseInput(const char *str) {
+  Node *operandList = NULL;
+  char numberTempStr[256] = {};
+  //   printf("%s\n", str); НУЖНО ПОТОМ УБРАТЬ
+
+  for (size_t i = 0, j = 0; str[i] != '\0'; i++) {
+    if ((str[i] >= '0' && str[i] <= '9') ||
+        (str[i] == '.' && isdigit(str[i - 1]))) {
+      numberTempStr[j] = str[i];
+      j++;
+    }
+    if ((!(str[i] >= '0' && str[i] <= '9') && (j && str[i] != '.')) ||
+        (str[i + 1] == '\0' && j)) {
+      push(&operandList, atof(numberTempStr), number, NO_PRIORITY);
+      bzero(&numberTempStr, strlen(numberTempStr));
+      j = 0;
+    }
+    if (str[i] == 'x') {
+      push(&operandList, 0, X, NO_PRIORITY);
+    }
+    if (str[i] == '+' || str[i] == '-' || str[i] == '*' || str[i] == '/' ||
+        str[i] == '%') {
+      if (str[i] == '+') {
+        if (str[0] == '+' && i == 0) {
+          push(&operandList, 0, number, NO_PRIORITY);
+        }
+        push(&operandList, 0, plus_t, LOW_PRIORITY);
+      }
+      if (str[i] == '-') {
+        if (str[0] == '-' && i == 0) {
+          push(&operandList, 0, number, NO_PRIORITY);
+        }
+        push(&operandList, 0, minus_t, LOW_PRIORITY);
+      }
+      if (str[i] == '*') {
+        push(&operandList, 0, mult_t, MEDIUM_PRIORITY);
+      }
+      if (str[i] == '/') {
+        push(&operandList, 0, div_tt, MEDIUM_PRIORITY);
+      }
+      if (str[i] == '%') {
+        push(&operandList, 0, mod_t, MEDIUM_PRIORITY);
+      }
+    } else if (str[i] == '^') {
+      push(&operandList, 3, 0, pow_t);
+    } else if (str[i] == 'c' || str[i] == 's' || str[i] == 't' ||
+               str[i] == 'a' || str[i] == 'l' || str[i] == 'm') {
+      if (str[i] == 'm' && str[i + 1] == 'o' && str[i + 2] == 'd') {
+        push(&operandList, 0, mod_t, HIGH_PRIORITY);
+        i = i + 2;
+      }
+      if (str[i] == 's' && str[i + 1] == 'q' && str[i + 2] == 'r' &&
+          str[i + 3] == 't') {
+        push(&operandList, 0, sqrt_t, HIGH_PRIORITY);
+        i = i + 3;
+      }
+      if (str[i] == 'c' && str[i + 1] == 'o' && str[i + 2] == 's') {
+        push(&operandList, 0, cos_t, HIGH_PRIORITY);
+        i = i + 2;
+      }
+      if (str[i] == 's' && str[i + 1] == 'i' && str[i + 2] == 'n') {
+        push(&operandList, 0, sin_t, HIGH_PRIORITY);
+        i = i + 2;
+      }
+      if (str[i] == 't' && str[i + 1] == 'a' && str[i + 2] == 'n') {
+        push(&operandList, 0, tan_t, HIGH_PRIORITY);
+        i = i + 2;
+      }
+      if (str[i] == 'c' && str[i + 1] == 'o' &&
+          str[i + 2] == 's') {  // не заходит
+        push(&operandList, 0, cos_t, HIGH_PRIORITY);
+        i = i + 2;
+      }
+      if (str[i] == 'a') {
+        if (str[i + 1] == 'c' && str[i + 2] == 'o' && str[i + 3] == 's') {
+          push(&operandList, 0, acos_t, HIGH_PRIORITY);
+          i = i + 3;
+        }
+        if (str[i + 1] == 's' && str[i + 2] == 'i' && str[i + 3] == 'n') {
+          push(&operandList, 0, asin_t, HIGH_PRIORITY);
+          i = i + 3;
+        }
+        if (str[i + 1] == 't' && str[i + 2] == 'a' && str[i + 3] == 'n') {
+          push(&operandList, 0, atan_t, HIGH_PRIORITY);
+          i = i + 3;
+        }
+      }
+      if (str[i] == 'l') {
+        if (str[i + 1] == 'n') {
+          push(&operandList, 0, ln_t, HIGH_PRIORITY);
+          i = i + 1;
+        }
+        if (str[i + 1] == 'o' && str[i + 2] == 'g') {
+          push(&operandList, 0, log_t, HIGH_PRIORITY);
+          i = i + 2;
+        }
+      }
+    } else if (str[i] == '(' || str[i] == ')') {
+      if (str[i] == '(') {
+        push(&operandList, 0, left_bracket_t, ULTRA_PRIORITY);
+        if (str[i + 1] == '+' || str[i + 1] == '-') {
+          push(&operandList, 0, number, NO_PRIORITY);
+        }
+      }
+      if (str[i] == ')') {
+        push(&operandList, 0, right_bracket_t, ULTRA_PRIORITY);
+      }
+    }
+  }
+  return operandList;
+}
+
+int main() {
+  char *s = "1+2+3-log(6)*sqrt(10)+ln(10)";
+  Node *list = NULL;
+  int rightBracket = 0, leftBracket = 0;
+  list = parseInput(s);
+
+  //   print(list);
+  //   printf("\n\n\n");
+  Node *tmp = inverseNode(list);
+  print(tmp);
+  //   push(&list, 14.48, tan_t, ULTRA_PRIORITY);
+  //   print(list);
+
+  return 0;
+}
