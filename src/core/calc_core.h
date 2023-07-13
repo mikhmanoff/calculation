a0e44054c0663eaf756133f8a4f7ca81e667f554
sallieba <sallieba@student.21-school.ru>
2023-07-13 15:39:53 +0300

    new funcs and changes

---
+++ src/core/calc_core.h
@@ -0,0 +1,5 @@
+#include "../backend/calc_support.h"
+
+Node *s21_infixToPolsih(Node *list, double xValue);
+double calculation(Node *list);
+double calculateOperands(double val1, double val2, funcType operation);
