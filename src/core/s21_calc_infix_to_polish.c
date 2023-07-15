adc0bc42fddb9db96d2511857d880beb19412d77
sallieba <sallieba@student.21-school.ru>
2023-07-15 17:20:56 +0300

    some changes

--- src/core/s21_calc_infix_to_polish.c
+++ src/core/s21_calc_infix_to_polish.c
@@ -6,6 +6,7 @@ Node *s21_infixToPolsih(Node *list, double xValue) {
   Node *polish = NULL;
 
   while (list) {
+    printf("!\n");
     if (operatorCheck(list->funType) || functionCheck(list->funType)) {
       if (stack == NULL) {
         push(&stack, list->num, list->funType, list->priority);
@@ -47,16 +48,17 @@ Node *s21_infixToPolsih(Node *list, double xValue) {
     }
 
     if (list->funType == right_bracket_t) {
-      if (stack != NULL) {
-        while (stack != NULL) {
-          push(&polish, stack->num, stack->funType, stack->priority);
-          pop(&stack);
-        }
+      while (stack->funType != left_bracket_t) {
+        printf("###################\n");
+        printf("%d\n", stack->funType);
+        printf("###################\n");
+        push(&polish, stack->num, stack->funType, stack->priority);
+        pop(&stack);
       }
+
       pop(&stack);
     }
-    // print(list);
-    // printf("\n\n\n");
+
     pop(&list);
   }
 
@@ -69,8 +71,20 @@ Node *s21_infixToPolsih(Node *list, double xValue) {
   return polish;
 }
 
+// void printStack(Node *stack) {
+//   if (list == NULL) {
+//     printf("NULL\n");
+//     exit(0);
+//   }
+//   for (Node *p = list; p != NULL; p = p->next) {
+//     printf("Приоритет - %d\nЧисло - %lf\nТип - %d\n-------------------\n",
+//            p->priority, p->num, p->funType);
+//   }
+//   printf("\n");
+// }
+
 int main() {
-  char str_1[200] = "12/(5*3)^2";  // -0.1+0.2
+  char str_1[200] = "acos(1)*asin(1)";  // -0.1+0.2
 
   Node *number_list = parseInput(str_1);
 
